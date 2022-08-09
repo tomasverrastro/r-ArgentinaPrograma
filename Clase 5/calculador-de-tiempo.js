b3a7c8703fd7df77b2fd2f9ae8b909fb14026efe
tomasverrastro <tomasvd.garoe@gmail.com>
2022-08-09 14:38:09 -0300

    Tareas clase 5
    Tarea corregida

--- Clase 5/calculador-de-tiempo.js
+++ Clase 5/calculador-de-tiempo.js
@@ -1,84 +1,62 @@
-
-
-
-const $botonCalcular = document.querySelector('#calcular');
+const $botonCalcular = document.querySelector('#calcular-tiempo-total');
 
 $botonCalcular.onclick = function(){
 
-    let horas1 = Number(document.querySelector('#horas2').value);
-    let minutos1 = Number(document.querySelector('#minutos2').value);
-    let segundos1 = Number(document.querySelector('#segundos2').value);
+    let horasVideo = document.querySelectorAll('.horas-video');
+    let minutosVideo = document.querySelectorAll('.minutos-video');
+    let segundosVideo = document.querySelectorAll('.segundos-video');
     
-    let horas2 = Number(document.querySelector('#horas3').value);
-    let minutos2 = Number(document.querySelector('#minutos3').value);
-    let segundos2 = Number(document.querySelector('#segundos3').value);
-    
-    let horas3 = Number(document.querySelector('#horas4').value);
-    let minutos3 = Number(document.querySelector('#minutos4').value);
-    let segundos3= Number(document.querySelector('#segundos4').value);
-    
-    let horas4 = Number(document.querySelector('#horas5').value);
-    let minutos4 = Number(document.querySelector('#minutos5').value);
-    let segundos4 = Number(document.querySelector('#segundos5').value);
-    
-    let horas5 = Number(document.querySelector('#horas6').value);
-    let minutos5 = Number(document.querySelector('#minutos6').value);
-    let segundos5 = Number(document.querySelector('#segundos6').value);
+    let horasSumadas = 0;
+    let minutosSumados = 0;
+    let segundosSumados = 0;
 
-    let horas6 = Number(document.querySelector('#horas1').value);
-    let minutos6 = Number(document.querySelector('#minutos1').value);
-    let segundos6 = Number(document.querySelector('#segundos1').value);
+    for (i = 0; i < horasVideo.length; i++){
 
-
-    function convertirASegundos1(horas1, minutos1, segundos1){
-
-        let segundosTotales = ((horas1 * 3600) + (minutos1 * 60) + (segundos1));
-        return segundosTotales;
-    }   
-    function convertirASegundos2(horas2, minutos2, segundos2){
-
-        let segundosTotales = ((horas2 * 3600) + (minutos2 * 60) + (segundos2));
-        return segundosTotales;
-    }   
-    function convertirASegundos3(horas3, minutos3, segundos3){
-
-        let segundosTotales = ((horas3 * 3600) + (minutos3 * 60) + (segundos3));
-        return segundosTotales;
-    }    
-    function convertirASegundos4(horas4, minutos4, segundos4){
-
-        let segundosTotales = ((horas4 * 3600) + (minutos4 * 60) + (segundos4));
-        return segundosTotales;
-    }   
-    function convertirASegundos5(horas5, minutos5, segundos5){
-
-        let segundosTotales = ((horas5 * 3600) + (minutos5 * 60) + (segundos5));
-        return segundosTotales;
-    }    
-    function convertirASegundos6(horas6, minutos6, segundos6){
-
-        let segundosTotales = ((horas6 * 3600) + (minutos6 * 60) + (segundos6));
-        return segundosTotales;
-        
+        horasSumadas = horasSumadas + Number(horasVideo[i].value);
     }
 
+    for (i = 0; i < minutosVideo.length; i++){
 
-    let tiempoTotal = convertirASegundos1(horas1, minutos1, segundos1) + convertirASegundos2(horas2, minutos2, segundos2) + 
-                      convertirASegundos3(horas3, minutos3, segundos3) + convertirASegundos4(horas4, minutos4, segundos4) + 
-                      convertirASegundos5(horas5, minutos5, segundos5) + convertirASegundos6(horas6, minutos6, segundos6);
-
-
-    let horasTotales = Math.floor(tiempoTotal / 3600);
-
-    let minutosTotales = Math.floor((tiempoTotal % 3600) / 60);
-
-    let segundosTotales = (tiempoTotal % 3600) % 60;
+        minutosSumados = minutosSumados + Number(minutosVideo[i].value);
+    }
     
+    for (i = 0; i < segundosVideo.length; i++){
+
+            segundosSumados = segundosSumados + Number(segundosVideo[i].value);
+    }
+    
+    function convertirASegundos(horasSumadas, minutosSumados, segundosSumados){
+
+        return ((horasSumadas * 3600) + (minutosSumados * 60) + (segundosSumados));
+    }   
+
+    let tiempoTotal = convertirASegundos(horasSumadas, minutosSumados, segundosSumados);
+
+    let horasTotales = 0;
+    let minutosTotales = 0;
+    let segundosTotales = 0;
+
+     while (tiempoTotal >= 3600)        {
+
+        horasTotales++; 
+        tiempoTotal -= 3600;
+     }   
+     
+     while (tiempoTotal >= 60){
+
+        minutosTotales++;
+        tiempoTotal -= 60;
+     }
+
+     while (tiempoTotal > 0) {
+
+        segundosTotales++
+        tiempoTotal--
+     }
 
     let resultado = `El tiempo total es ${horasTotales} horas, ${minutosTotales} minutos y ${segundosTotales} segundos.`;
     
     document.querySelector('#tiempo-total').innerText = resultado;
-
 
 return false;
 }
